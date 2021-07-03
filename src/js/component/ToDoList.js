fcdc84dfe8b4e2ae374d3983b7c6dfe59446472e
plotier <plotier15@gmail.com>
2021-07-03 10:42:22 +0000

    todo list w fetch terminado

--- src/js/component/ToDoList.js
+++ src/js/component/ToDoList.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const ToDoList = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [taskList, setTaskList] = useState([]);
+	const url = "https://assets.breatheco.de/apis/fake/todos/user/plotier";
+	useEffect(() => {
+		fetch(url)
+			.then(response => response.json()) //Header o saber qué pasó con llamada
+			.then(data => {
+				// Se procesa la información
+				setTaskList(data);
+			});
+	}, []);
+	useEffect(() => {
+		fetch(url, {
+			method: "PUT",
+			body: JSON.stringify(taskList),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}, [taskList]);
 	const listaItems = taskList.map((item, index) => {
 		return (
 			<div key={index}>
 				<li className="Example list-group-item d-flex justify-content-between">
-					{item}
+					{item.label}
 					<i
 						className="fas fa-times"
 						onClick={() => {
@@ -27,7 +45,13 @@ export const ToDoList = () => {
 				onChange={e => setInputValue(e.target.value)}
 				value={inputValue}
 			/>
-			<button onClick={e => setTaskList([...taskList, inputValue])}>
+			<button
+				onClick={e =>
+					setTaskList([
+						...taskList,
+						{ label: inputValue, done: false }
+					])
+				}>
 				Add
 			</button>
 			<ul className="list-group">{listaItems}</ul>
